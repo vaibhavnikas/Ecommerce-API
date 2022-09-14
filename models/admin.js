ca5ed73b3da6ea43ba6f1304a794327f6250b86d
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-14 23:05:51 +0530

    create admin model

---
+++ models/admin.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// define adminSchema
+const adminSchema = new mongoose.Schema({
+    email: {
+        type : String,
+        required : true,
+        unique: true
+    },
+    password: {
+        type : String,
+        required : true
+    }
+},{
+    timestamps : true
+});
+
+// create admin model to store admin's info in database
+const Admin = mongoose.model('Admin', adminSchema);
+
+// export user model
+module.exports = Admin;
