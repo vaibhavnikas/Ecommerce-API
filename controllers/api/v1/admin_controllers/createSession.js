f36018b89a02f9cac72ea1724cfbc28cd6ffa0eb
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-16 00:52:47 +0530

    moved all functions from admin_api.js to admin_controllers folder to make it scalable

---
+++ controllers/api/v1/admin_controllers/createSession.js
@@ -0,0 +1,34 @@
+const Admin = require('../../../../models/admin');
+const jwt = require('jsonwebtoken');
+
+module.exports = async function(req, res){
+    try{
+        if(!(req.body.email && req.body.password)){
+            return res.status(401).json({
+                message: "Please enter email and password to create session"
+            });
+        }
+
+        let admin = await Admin.findOne({email: req.body.email});
+
+        if(!admin || admin.password != req.body.password){
+            return res.status(422).json({
+                message: "Invalid email or password"
+            });
+        }else{
+            return res.status(200).json({
+                message: "Session created successfully !",
+                data: {
+                    message: 'Here is your token, please keep it safe',
+                    token: jwt.sign(admin.toJSON(), 'changeBeforeProduction', {expiresIn: '10000000'})
+                }
+            });
+        }
+
+    }catch(err){
+        console.log('Error :', err);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+}
---
+++ controllers/api/v1/admin_controllers/register.js
@@ -0,0 +1,32 @@
+const Admin = require('../../../../models/admin');
+
+module.exports = async function(req, res){
+    try{
+
+        if(!(req.body.email && req.body.password)){
+            return res.status(401).json({
+                message: "Please enter email and password to complete the registration"
+            });
+        }
+
+        let admin = await Admin.findOne({email:req.body.email});
+
+        if(!admin){
+            await Admin.create(req.body);
+
+            return res.status(200).json({
+                message: "Registration successful"
+            });
+        }else{
+            return res.status(409).json({
+                message: "Email address is already registered"
+            });
+        }
+        
+    }catch(err){
+        console.log('Error :', err);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+}
