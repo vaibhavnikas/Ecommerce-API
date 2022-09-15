7a3e8d618dac470afb6a429815d838db378ff862
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-16 00:15:15 +0530

    add route and controller to update product quantity

--- controllers/api/v1/product_api.js
+++ controllers/api/v1/product_api.js
@@ -85,7 +85,7 @@ module.exports.delete = async function(req, res){
 
         if(!product){
             return res.status(200).json({
-                message: 'Invalid id !'
+                message: 'Invalid product-id !'
             });
         }
 
@@ -93,6 +93,53 @@ module.exports.delete = async function(req, res){
 
         return res.status(200).json({
             message: 'product deleted'
+        });
+    }catch(err){
+        console.log('Error :');
+        console.log('*:', err);
+        return res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+}
+
+
+module.exports.updateProductQuantity = async function(req, res){
+    try{
+        if(req.params.id.length != 24){ 
+            req.params.id = parseInt(req.params.id); 
+        }
+        let _id = mongoose.Types.ObjectId(req.params.id);
+        let product = await Product.findById(_id);
+
+        if(!product){
+            return res.status(200).json({
+                message: 'Invalid product-id !'
+            });
+        }
+
+        if(!req.query.number){
+            return res.status(200).json({
+                message: 'Please enter number in query params to update quantity'
+            })
+        }
+
+        if(parseInt(req.query.number) < 0){
+            return res.status(200).json({
+                message: 'Product quantity cannot be negative'
+            });
+        }
+
+        product.quantity = parseInt(req.query.number);
+        product.save();
+
+        return res.status(200).json({
+            message: 'quantity updated successfully',
+            data: {
+                id: product.id,
+                name: product.name,
+                quantity: product.quantity
+            }
         });
     }catch(err){
         console.log('Error :');
--- routes/api/v1/product_api.js
+++ routes/api/v1/product_api.js
@@ -5,5 +5,6 @@ const productsController = require('../../../controllers/api/v1/product_api');
 router.post('/create', productsController.create);
 router.get('/', productsController.displayProducts);
 router.delete('/:id', productsController.delete);
+router.post('/:id/update_quantity', productsController.updateProductQuantity);
 
 module.exports = router;
