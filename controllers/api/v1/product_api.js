ab7faad0a246290b40d31047e2cc7e9319771946
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-15 22:22:15 +0530

    add route and controller to display products

--- controllers/api/v1/product_api.js
+++ controllers/api/v1/product_api.js
@@ -25,12 +25,43 @@ module.exports.create = async function(req, res){
         const newProduct = await Product.create(req.body);
 
         return res.status(200).json({
-            data: {
+            message: 'Product added successfully !',
+            data: {  
                 product: {
                     name: newProduct.name,
                     quantity: newProduct.quantity
-                },
-                message: 'Product added successfully'
+                }
+            }
+        });
+    }catch(err){
+        console.log('Error :');
+        console.log('*:', err);
+        return res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+}
+
+
+module.exports.displayProducts = async function(req, res){
+    try{
+        let products = await Product.find();
+        let message = 'Here is a list of all the products';
+
+        products = products.map((product)=>({
+            id: product.id,
+            name: product.name,
+            quantity: product.quantity
+        }));
+
+        if(products.length == 0){
+            message = 'There are no products to display !'
+        }
+
+        return res.status(200).json({
+            message,
+            data: {
+                products
             }
         });
     }catch(err){
--- routes/api/v1/product_api.js
+++ routes/api/v1/product_api.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productsController = require('../../../controllers/api/v1/product_api');
 
 router.post('/create', productsController.create);
+router.get('/', productsController.displayProducts);
 
 module.exports = router;
